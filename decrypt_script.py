7bb2e4e839c1b1d1a52fbca94802ae5af33d9156
Qais Elyamani <qays157@hormail.com>
2023-12-27 09:31:06 +0100

    here, a seperate encrypt and decrypt scripts are created

---
+++ decrypt_script.py
@@ -0,0 +1,63 @@
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+def load_key_from_file(filename, is_private=True):
+    with open(filename, 'rb') as f:
+        key_bytes = f.read()
+
+    if is_private:
+        return serialization.load_pem_private_key(
+            key_bytes,
+            password=None,
+            backend=default_backend()
+        )
+    else:
+        return serialization.load_pem_public_key(
+            key_bytes,
+            backend=default_backend()
+        )
+
+def decrypt_file(encrypted_file_path, private_key):
+    print(f"Decrypting file: {encrypted_file_path}")
+    with open(encrypted_file_path, 'rb') as f:
+        encrypted_data = f.read()
+
+    encrypted_symmetric_key = encrypted_data[:private_key.key_size // 8]
+    ciphertext = encrypted_data[private_key.key_size // 8:]
+
+    symmetric_key = private_key.decrypt(
+        encrypted_symmetric_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    cipher = Cipher(algorithms.AES(symmetric_key), modes.OFB(symmetric_key[:16]), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+
+    # Extract the filename from the path
+    _, filename = os.path.split(encrypted_file_path)
+    decrypted_filename = 'decrypted_' + filename[:-4]  # Remove '.enc' and add 'decrypted_'
+
+    decrypted_file_path = os.path.join(os.path.dirname(encrypted_file_path), decrypted_filename)
+
+    with open(decrypted_file_path, 'wb') as f:
+        f.write(decrypted_data)
+
+    print(f"Decryption successful. Decrypted file: {decrypted_file_path}")
+
+if __name__ == "__main__":
+    private_key = load_key_from_file('private_key.pem')
+
+    folder_path = r'C:\Users\Qais AL-yamani\Documents\TPM_project2\sensitive_data'
+    encrypted_files = [f for f in os.listdir(folder_path) if f.endswith('.enc')]
+
+    for encrypted_file in encrypted_files:
+        encrypted_file_path = os.path.join(folder_path, encrypted_file)
+        decrypt_file(encrypted_file_path, private_key)
---
+++ encrypt_script.py
@@ -0,0 +1,74 @@
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+def generate_key_pair():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+
+    return private_key, public_key
+
+def save_key_to_file(key, filename, is_private=True):
+    if is_private:
+        key_bytes = key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+    else:
+        key_bytes = key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+    with open(filename, 'wb') as f:
+        f.write(key_bytes)
+
+def encrypt_file(file_path, public_key):
+    print(f"Encrypting file: {file_path}")
+    with open(file_path, 'rb') as f:
+        plaintext = f.read()
+
+    symmetric_key = os.urandom(32)
+    encrypted_symmetric_key = public_key.encrypt(
+        symmetric_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # Use algorithms.AES(symmetric_key) to create an instance of the AES algorithm
+    aes_algorithm = algorithms.AES(symmetric_key)
+    
+    # Use modes.CFB(symmetric_key[:16]) for CFB mode
+    cipher = Cipher(aes_algorithm, modes.CFB(symmetric_key[:16]), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+    encrypted_file_path = file_path + '.enc'
+    with open(encrypted_file_path, 'wb') as f:
+        f.write(encrypted_symmetric_key + ciphertext)
+
+    print(f"Encryption successful. Encrypted file: {encrypted_file_path}")
+
+if __name__ == "__main__":
+    private_key, public_key = generate_key_pair()
+    
+    save_key_to_file(private_key, 'private_key.pem')
+    save_key_to_file(public_key, 'public_key.pem', is_private=False)
+
+    folder_path = r'C:\Users\Qais AL-yamani\Documents\TPM_project2\sensitive_data'
+    files_to_encrypt = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+    for file_to_encrypt in files_to_encrypt:
+        file_path = os.path.join(folder_path, file_to_encrypt)
+        encrypt_file(file_path, public_key)
